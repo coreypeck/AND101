498e182e77d6b15ed6b23558464f628d10e3a6b7
Corey Peck <coreypeck@gmail.com>
2017-07-01 21:43:07 -0500

    Add project files.

---
+++ TipCalculator/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
+
+            void OnCalculateClick(object sender, EventArgs e)
+            {
+                string text = inputBill.Text;
+
+                var total = Convert.ToDouble(text);
+
+                var tip = total * .15;
+
+                tip = Math.Round(tip, 2);
+
+                outputTip.Text = tip.ToString();
+
+                outputTotal.Text = (Math.Round(total, 2) + tip).ToString();
+            }
+
+        }
+    }
+}
+
